b7d1507623b2a915e18d2d9f0fa2140f08884492
Sektor20fast <xorev.lexa@gmail.com>
2026-01-28 20:22:05 +0300

    Delete app directory

--- app/src/main/java/com/example/mvd1/FaqItem.java
+++
@@ -1,9 +0,0 @@
-package com.example.mvd1;
-
-public class FaqItem {
-    private String question;
-    private String answer;
-
-    public String getQuestion() { return question; }
-    public String getAnswer() { return answer; }
-}
